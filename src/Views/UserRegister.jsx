e63dd519a3c4509a66f092f8ab2e03f9e17b64e9
Ales <aleswebgit@gmail.com>
2022-07-27 09:30:26 +0200

    quit navbar and arrow on register form

--- src/Views/UserRegister.jsx
+++ src/Views/UserRegister.jsx
@@ -1,13 +1,9 @@
 import UserRegisterForm from '../Components/UserRegisterForm'
-import Arrow from '../media/icons/Arrow'
-import Navbar from '../Components/Navbar'
 
 function UserRegister(){
   return(
     <>
-      <Navbar/>
       <UserRegisterForm/>
-      <Arrow/>
     </>
   )
 }
